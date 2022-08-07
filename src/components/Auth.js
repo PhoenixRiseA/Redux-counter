f3fc4337be1ed061fe9de17ef555829f0245b34c
John Doe <sailalith1993@gmail.com>
2022-08-07 20:28:36 +0530

    Auth Reducer

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,21 +1,35 @@
-import classes from './Auth.module.css';
+import classes from "./Auth.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../store/index";
 
 const Auth = () => {
+  const dispatch = useDispatch();
+  const isLogin = useSelector((state) => state.auth.isAuthenticated);
+  console.log(isLogin);
+  const loginHandler = (e) => {
+    e.preventDefault();
+    console.log("in login");
+    dispatch(authActions.login());
+  };
+
   return (
     <main className={classes.auth}>
-      <section>
-        <form>
-          <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
-          </div>
-          <button>Login</button>
-        </form>
-      </section>
+      {!isLogin && (
+        <section>
+          <form>
+            <div className={classes.control}>
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <button onClick={loginHandler}>Login</button>
+          </form>
+        </section>
+      )}
+      {isLogin && <h1>My user profile</h1>}
     </main>
   );
 };
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,33 +1,37 @@
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/index";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
-  const incrementBy5Handler = () => {
-    dispatch({ type: "incrementBy5" });
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(5));
   };
-  const decrementBy5Handler = () => {
-    dispatch({ type: "decrementBy5" });
+  const decreaseHandler = () => {
+    dispatch(counterActions.decrease(5));
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
       <button onClick={incrementHandler}>Increment</button>
       <button onClick={decrementHandler}>Decrement</button>
-      <button onClick={incrementBy5Handler}>Increment by 5</button>
-      <button onClick={decrementBy5Handler}>Decrement by 5</button>
+      <button onClick={increaseHandler}>Increment by 5</button>
+      <button onClick={decreaseHandler}>Decrement by 5</button>
     </main>
   );
 };
